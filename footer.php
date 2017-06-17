fa031db5dac635544c54a0e7b96d68ab8cf6f4e9
Razvan Nica <razvan.nica30@gmail.com>
2017-06-17 21:58:41 +0300

    Delete footer.php

--- footer.php
+++
@@ -1,3 +0,0 @@
-<div class="footer">
-    <p class="copy">Copyright &copy; 2017 <span>|</span> A "Semi-Barbosi" project <span>|</span> DETON - Detention Monitoring Tool <span>|</span> Contact information: <a href="mailto:tudor_antonie@yahoo.com">tudor_antonie@yahoo.com</a></p>
-</div>
