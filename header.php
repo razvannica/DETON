3c728523f5a5a1bf887348290dcd050691e678e3
Razvan Nica <razvan.nica30@gmail.com>
2017-06-17 21:52:02 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,19 @@
+<div class="header">
+
+    <h1 id="logo">
+        <li><a href="index.php">Detention Monitoring Tool</a></li>
+    </h1>
+
+    <nav id="navigation">
+        <ul>
+
+            <li><a href="prisoners.php">Prisoners</a></li>
+            <li><a href="prisons.php">Prisons</a></li>
+            <li><a href="statistics.php">Statistics</a></li>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
+
+</div>
+
---
+++ prisons.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title> D E T O N </title>
+    <link rel="shortcut icon" type="image/x-icon" href="../images/icon.png"/>
+    <link rel="stylesheet" href="../css/style.css" type="text/css"/>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+        td, th {
+            border: 1px solid #ff6600;
+            text-align: right;
+            padding: 8px;
+        }
+    </style>
+</head>
+<body>
+<!-- wrapper -->
+<div id="wrapper">
+    <div class="light-bg">
+        <!-- shell -->
+        <div class="shell">
+            <!-- header -->
+            <?php require "header.php" ?>
+            <!-- end of header -->
+            <!-- main -->
+            <div class="main">
+                <!-- content -->
+                <section class="content">
+                    <!-- post -->
+                    <div class="post">
+                        <!-- post-inner -->
+                        <div class="post-inner">
+
+
+                            <table>
+                                <tr>
+                                    <th><strong> Prison name </strong></th>
+                                    <th><strong> Capacity </strong></th>
+                                    <th><strong> Population </strong></th>
+                                    <th><strong> Address </strong></th>
+                                </tr>
+                                <tr>
+                                    <td>Fox River</td>
+                                    <td>1246</td>
+                                    <td>2361</td>
+                                    <td>Illinois, Pennsylvanya</td>
+                                </tr>
+                                <tr>
+                                    <td>Sona</td>
+                                    <td>2654</td>
+                                    <td>5342</td>
+                                    <td>Mexico, Los Aucantos</td>
+                                </tr>
+                                <tr>
+                                    <td>Ernst Handel</td>
+                                    <td>467</td>
+                                    <td>768</td>
+                                    <td>Austria</td>
+                                </tr>
+                                <tr>
+                                    <td>Island Trading</td>
+                                    <td>7654</td>
+                                    <td>874</td>
+                                    <td>UK</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <!-- post-inner -->
+                    </div>
+                    <!-- end of post -->
+
+                    <!-- end of post -->
+                </section>
+                <!-- end of content -->
+                <!-- sidebar -->
+
+
+                <!-- end of sidebar -->
+                <div class="cl">&nbsp;</div>
+            </div>
+            <!-- end of main -->
+            <?php require('footer.php'); ?>
+        </div>
+        <!-- end of shell -->
+    </div>
+</div>
+<!-- end of wrapper -->
+</body>
+</html>
