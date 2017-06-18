e4306e72ac2b142737fb17b7b7c0e9c2f582cdde
anttudor <andrei.antonie@info.uaic.ro>
2017-06-18 13:33:16 +0300

    Add files via upload

---
+++ front-end/header_logged.php
@@ -0,0 +1,19 @@
+<div class="header">
+
+    <h1 id="logo">
+        <li><a href="index.php">Detention Monitoring Tool</a></li>
+    </h1>
+
+    <nav id="navigation">
+        <ul>
+
+            <li><a href="prisoners.php">Prisoners</a></li>
+            <li><a href="prisons.php">Prisons</a></li>
+            <li><a href="statistics.php">Statistics</a></li>
+            <li><a href="Visits.php">Visits</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </nav>
+
+</div>
+
