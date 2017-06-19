89219ee45f11483fb5222d37965225b1f2783dcf
anttudor <andrei.antonie@info.uaic.ro>
2017-06-19 21:30:55 +0300

    Update AddVisits.php

--- front-end/AddVisits.php
+++ front-end/AddVisits.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title> D E T O N </title>
-    <link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+    <link rel="shortcut icon" type="image/x-icon" href="images/icon.png" />
     <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
     <link rel="stylesheet" href="css/colorbox.css" type="text/css" media="all" />
 </head>
@@ -26,26 +26,18 @@
                         <div class="post-inner">
                             <div class="post-cnt">
                                 <form action="AddVisits.php" method="post">
+                                    <h3 style="color: #1cfe10">Edit Visit</h3>
                                     <table style="width:100%">
-                                        <tr><td>Prisoner ID: </td><td>
-                                                <input type="number" name="convict_id" style="width:250px; border:2px solid ;"/></td></tr>
-                                        <tr><td colspan="2" style="height:10px"></td></tr>
-                                        <tr><td>Last Name: </td><td>
-                                                <input type="text" name="first_name" style="width:250px; border:2px solid ;"/></td></tr>
-                                        <tr><td colspan="2" style="height:10px"></td></tr>
-                                        <tr><td>Middle Name: </td><td>
-                                                <input type="text" name="middle_name" style="width:250px; border:2px solid ;"/></td></tr>
-                                        <tr><td colspan="2" style="height:10px"></td></tr>
-                                        <tr><td>First Name: </td><td>
-                                                <input type="text" name="first_name" style="width:250px; border:2px solid ;"/></td></tr>
-                                        <tr><td colspan="2" style="height:10px"></td></tr>
-                                        <tr><td>Prison Name: </td><td>
-                                                <input type="text" name="prison_name" style="width:250px; border:2px solid ;"/></td></tr>
-                                        <tr><td colspan="2" style="height:10px"></td></tr>
-                                        <tr><td>Date of visit: </td><td>
+                                        <tr><td>New date of visit: </td><td>
                                                 <input type="date" name="visit_date" style="width:250px; border:2px solid ;"/></td></tr>
                                         <tr><td colspan="2" style="height:10px"></td></tr>
-                                            <br><br>
+                                        <tr><td>New visit reason: </td><td>
+                                                <input type="text" name="visit_reason" style="width:250px; border:2px solid ;"/></td></tr>
+                                        <tr><td colspan="2" style="height:10px"></td></tr>
+                                        <tr><td>New received objectes: </td><td>
+                                                <input type="text" name="exchange_data" style="width:250px; border:2px solid ;"/></td></tr>
+                                        <tr><td colspan="2" style="height:10px"></td></tr>
+                                        <br><br>
                                         <tr><td colspan="2" align="center"><input type="submit" value="Submit" style="height:30px; width: 60px;"/></td></tr>
                                     </table>
                                 </form>
